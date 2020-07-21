b6e94e536205a56f7bebad1e2efb35f657114658
adar tzeiri <adar-tz@yit.co.il>
2020-07-21 12:59:01 +0300

    Initial Commit

---
+++ LinkTextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkTextView
+//
+//  Created by Adar Tzeiri on 21/07/2020.
+//  Copyright © 2020 Adar Tzeiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
